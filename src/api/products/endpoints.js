46e56f6d150000c55c41d27ebbe065ed6bf7abf8
Nina <nino-25@mail.ru>
2022-04-19 19:19:35 +0400

    Add console log

--- src/api/products/endpoints.js
+++ src/api/products/endpoints.js
@@ -11,7 +11,7 @@ const headersOptions = {
 
 const getProductsPerCategories = async (category, limit) => {
   const { data } = await axios.get(
-    `/products?category=${category}&limit=${limit}&fields=images,name,price`
+    `https://mern-ecommerce-node.herokuapp.com/api/v1/products?category=${category}&limit=${limit}&fields=images,name,price`
   );
   console.log(data)
   return data;
